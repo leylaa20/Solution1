33f71494269ba8be05ee16041fefcad2eb2febe2
Qoca_rh86 <Qoca_rh86@STHQ012E-18>
2022-07-06 22:01:27 +0400

    Add project files.

---
+++ Factory/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Factory;
+
+interface IButton
+{
+    void Render();
+    void OnClick();
+}
+
+class Windows : IButton
+{
+    public void OnClick() => Console.WriteLine("Windows OnClick");
+
+    public void Render() => Console.WriteLine("Windows Render");
+}
+
+
+class HTML : IButton
+{
+    public void OnClick() => Console.WriteLine("HTML OnClick");
+
+    public void Render() => Console.WriteLine("HTML Render");
+}
+
+
+abstract class Dialog
+{
+    public void Render() { Console.WriteLine("Render"); }
+    public abstract IButton CreateButton();
+}
+
+
+class WindowsDialog : Dialog
+{
+    public override IButton CreateButton() => new Windows();
+}
+
+
+class HTMLDialog : Dialog
+{
+    public override IButton CreateButton() => new HTML();
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        IButton? button = null;
+        Dialog? dialog = null;
+
+        while (true)
+        {
+            Console.WriteLine("1) Windows");
+            Console.WriteLine("2) HTML");
+            Console.WriteLine("0) Exit");
+            Console.Write("\n\tEnter choice ");
+
+            button = Console.ReadLine() switch
+            {
+                "1" => new Windows(),
+                "2" => new HTML(),
+                _ => null
+            };
+
+            button = dialog?.CreateButton();
+            dialog?.Render();
+        }
+    }
+}
